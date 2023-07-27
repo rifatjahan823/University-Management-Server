6d8fe2f12e4c640b21c3a226e75d068138817dfa
rifatjahan823 <rifatjahan823@gmail.com>
2023-07-27 07:41:28 +0600

    Setup Express,Mongose,Cors,Parser

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+//midleware-----------
+const app:Application = express()
+app.use(cors())
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+
+app.get('/', (req:Request, res:Response) => {
+  res.send('Hello World!')
+})
+
+
+export default app
