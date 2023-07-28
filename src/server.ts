a436090ae8bdbeb2f0f6aea7a79c7901d6c18009
rifatjahan823 <rifatjahan823@gmail.com>
2023-07-28 11:25:31 +0600

    lint

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,16 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
-
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function main() {
- try{
-    await mongoose.connect(config.database_url as string);
+  try {
+    await mongoose.connect(config.database_url as string)
     app.listen(config.port, () => {
       console.log(`Database Connected ${config.port}`)
     })
- }catch(err){
+  } catch (err) {
     console.log('Database not connected')
- }
+  }
 }
 
-main().catch(err => console.log(err));
+main().catch(err => console.log(err))
